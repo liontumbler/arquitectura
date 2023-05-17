a0334df95dea0e5aacfdcfa00d25c537d4f42d04
edwin velasquez <liontumbler@gmail.com>
2023-05-17 17:54:30 -0500

    arreglos

--- view/layout/nav.php
+++ view/layout/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow mb-5">
     <div class="container-fluid">
         <a href="javascript:" class="navbar-brand" id="btnSidebar">
             <img src="resources/img/adminLig.svg" style="width: 50px;">
